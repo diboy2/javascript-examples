39f4a8e2a49e3c9fcf0bfa5f272bc4f154d59f2c
Cagaanan <AC044401@cerner.net>
2018-02-03 14:46:37 -0600

    Added array classes

--- examples/array.js
+++ examples/array.js
@@ -18,7 +18,30 @@ const sequentialSearch = (arr, value) => {
     return undefined;
 };
 
+const binarySearch = (arr, value) => {
+    var arrayLength = arr.length;
+    var low = 0;
+    var high = arrayLength - 1;
+
+    while(low <= high){
+        let mid = low + Math.floor((high - low) / 2);
+
+        if(arr[mid] === value){
+            return mid;
+        }
+
+        if(arr[mid] < value){
+            low = mid + 1;
+        }
+        if(arr[mid] > value){
+            high = mid - 1;
+        }
+    }
+    return -1;
+};
+
 export {
     sumArray,
-    sequentialSearch
+    sequentialSearch,
+    binarySearch
 };
--- spec/array.spec.js
+++ spec/array.spec.js
@@ -1,4 +1,4 @@
-import {sumArray, sequentialSearch} from "../examples/array";
+import {sumArray, sequentialSearch, binarySearch} from "../examples/array";
 
 describe("Array Interview Questions", () => {
     it("sum all the elements of an array", ()=> {
@@ -9,6 +9,14 @@ describe("Array Interview Questions", () => {
     it("search an array for a given value", () => {
         let arr = [5,4,3,2,1];
         expect(sequentialSearch(arr, 2)).toBe(3);
-    })
+    });
+
+    it("use binary search to find the index of a given value", () => {
+        let arr = [1,2,3,4,5,6,7,8,9,10];
+        expect(binarySearch(arr,4 )).toBe(3);
+        expect(binarySearch(arr, 7)).toBe(6);
+    });
+
 
 });
+
